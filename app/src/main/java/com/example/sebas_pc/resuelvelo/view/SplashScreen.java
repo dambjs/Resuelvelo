7c6728c075fe345b9ec51d95a7e4205caf949dd9
esvastic <stebar@iespuigcastellar.xeill.net>
2018-04-20 19:47:21 +0200

    Splash

---
+++ app/src/main/java/com/example/sebas_pc/resuelvelo/view/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.example.sebas_pc.resuelvelo.view;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+
+import com.example.sebas_pc.resuelvelo.R;
+
+
+public class SplashScreen extends AppCompatActivity {
+    TextView tvSplash;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //menghilangkan ActionBar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.splash_screen);
+
+        tvSplash = (TextView) findViewById(R.id.tvSplash);
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), Logueo.class));
+                finish();
+            }
+        }, 3000L); //3000 L = 3 detik
+    }
+}
