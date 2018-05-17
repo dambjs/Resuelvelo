118a7a09344463d1f6f2e9beee82d03a7bf1c26c
dambjs <dambjs@gmail.com>
2018-05-18 00:11:06 +0200

    Resuelvelo 1.0

---
+++ app/src/main/java/com/example/sebas_pc/resuelvelo/model/FotoPersonal.java
@@ -0,0 +1,13 @@
+package com.example.sebas_pc.resuelvelo.model;
+
+public class FotoPersonal {
+    public String userId;
+    public String imagenP;
+
+    public FotoPersonal(String userId, String imagenP) {
+        this.userId = userId;
+        this.imagenP = imagenP;
+    }
+
+    public FotoPersonal(){}
+}
--- app/src/main/java/com/example/sebas_pc/resuelvelo/view/PerfilEmpresario.java
+++ app/src/main/java/com/example/sebas_pc/resuelvelo/view/PerfilEmpresario.java
@@ -3,39 +3,55 @@ package com.example.sebas_pc.resuelvelo.view;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.example.sebas_pc.resuelvelo.R;
 import com.example.sebas_pc.resuelvelo.model.Empresa;
+import com.example.sebas_pc.resuelvelo.model.FotoPersonal;
 import com.example.sebas_pc.resuelvelo.model.User;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class PerfilEmpresario extends AppCompatActivity {
 
     private TextView nom;
     private TextView correo;
-    private DatabaseReference mDatabase, mDatabase2;
+    private ImageView imagenP;
+    private TextView txt1;
+    private DatabaseReference mDatabase, mDatabase2, mDatabase3;
     private FirebaseRecyclerAdapter mAdapter;
-
+    Uri mediaUri;
+    Uri downloaderUrl;
+    private final int RC_IMAGE_PICK = 5677;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,10 +62,12 @@ public class PerfilEmpresario extends AppCompatActivity {
 
         mDatabase = FirebaseDatabase.getInstance().getReference().child("users").child(uid);
         mDatabase2 = FirebaseDatabase.getInstance().getReference().child("empresa").child(uid);
+//        mDatabase3 = FirebaseDatabase.getInstance().getReference().child("imagenPersonal").child(uid);
 
         correo = findViewById(R.id.email);
         nom = findViewById(R.id.displayNameEmpresa);
-
+        imagenP = findViewById(R.id.imagenP);
+        txt1 = findViewById(R.id.txt1);
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -106,8 +124,78 @@ public class PerfilEmpresario extends AppCompatActivity {
 
             }
         };
-
         recyclerView.setAdapter(mAdapter);
+
+//        imagenP.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                imagenP.setEnabled(false);
+//                if (mediaUri != null){
+//                    uploadFile();
+//                } else{
+//                    creaImagen();
+//                }
+//                finish();
+//            }
+//        });
+//        cargarFoto();
+    }
+
+
+//    void uploadFile(){
+//        StorageReference fileRef = FirebaseStorage.getInstance().getReference().child("ImagenPersonal/" + nom.getText());
+//        fileRef.putFile(mediaUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//            @Override
+//            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                downloaderUrl = taskSnapshot.getDownloadUrl();
+//                creaImagen();
+//            }
+//        });
+//    }
+
+//    private void creaImagen() {
+//        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//        if(downloaderUrl == null) {
+//            mDatabase3.setValue(new FotoPersonal(user.getUid(), null));
+//        } else {
+//            mDatabase3.setValue(new FotoPersonal(user.getUid(), downloaderUrl.toString()));
+//        }
+//        finish();
+//    }
+
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        if (data != null) {
+//            if (requestCode == RC_IMAGE_PICK) {
+//                mediaUri = data.getData();
+//                Glide.with(this).load(mediaUri).into(imagenP);
+//            }
+//        }
+//    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_imagen, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home: //hago un case por si en un futuro agrego mas opciones
+                Log.i("ActionBar", "Atrás");
+                finish();
+                return true;
+            case R.id.action_settings:
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,RC_IMAGE_PICK);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 
     public void salir(View view) {
@@ -135,6 +223,25 @@ public class PerfilEmpresario extends AppCompatActivity {
                 .setNegativeButton("No", null)
                 .show();
     }
+
+//    void cargarFoto(){
+//        mDatabase3.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                FotoPersonal fotoPersonal = dataSnapshot.getValue(FotoPersonal.class);
+//                if (fotoPersonal != null) {
+//                    Glide.with(PerfilEmpresario.this)
+//                            .load(fotoPersonal.imagenP)
+//                            .into(imagenP);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
 
 
     public void add(View view) {
