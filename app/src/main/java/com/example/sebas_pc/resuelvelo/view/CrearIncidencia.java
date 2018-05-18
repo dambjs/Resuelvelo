19f00701821696771975546e7ddbc2808b533ca8
dambjs <dambjs@gmail.com>
2018-05-18 19:54:47 +0200

    Resuelvelo 1.0

--- app/src/main/java/com/example/sebas_pc/resuelvelo/view/CrearIncidencia.java
+++ app/src/main/java/com/example/sebas_pc/resuelvelo/view/CrearIncidencia.java
@@ -1,24 +1,33 @@
 package com.example.sebas_pc.resuelvelo.view;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
 import android.database.DataSetObserver;
 import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.sebas_pc.resuelvelo.R;
+import com.example.sebas_pc.resuelvelo.model.Empresa;
 import com.example.sebas_pc.resuelvelo.model.Incidencia;
 import com.example.sebas_pc.resuelvelo.model.User;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -26,16 +35,26 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class CrearIncidencia extends AppCompatActivity {
 
 //    private String dropDownItemArr[] = {"Prioridad Baja", "Prioridad Media", "Prioridad Alta"};
-    private ImageView image;
-    private ActionBar actionBar;
-    private DatabaseReference mDatabase, mDatabase2;
+    private ImageView image, add;
+    private Button enviar;
+    private Spinner jaja;
+    private Spinner areaSpinner2;
+    private Spinner areaSpinner1;
+    Uri mediaUri;
+    Uri downloaderUrl;
+    private final int RC_IMAGE_PICK = 5677;
+    private DatabaseReference mDatabase, mDatabase2, mDatabase3;
     private final static String[] letra = { "Fallo de impresora", "Fallo de Sistema Operativo", "Falla el ordenador",
             "Pantalla sin señal", "Proyector estropeado", "Otros" };
 
@@ -46,11 +65,37 @@ public class CrearIncidencia extends AppCompatActivity {
         setContentView(R.layout.activity_crear_incidencia);
 
         String uid = FirebaseAuth.getInstance().getUid();
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("departamentos").child(uid);
-        mDatabase2 = FirebaseDatabase.getInstance().getReference().child("usuariosM");
+        enviar = findViewById(R.id.enviar);
+        add = findViewById(R.id.add);
         image = findViewById(R.id.image);
 
-        Spinner jaja = (Spinner) findViewById(R.id.sp3);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("departamentos").child(uid);
+        mDatabase2 = FirebaseDatabase.getInstance().getReference().child("usuariosM");
+        mDatabase3 = FirebaseDatabase.getInstance().getReference().child("incidencia").child(uid);
+
+
+        enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                enviar.setEnabled(false);
+                if (mediaUri != null){
+                    uploadFile();
+                } else{
+                    incidencia();
+                }
+            }
+        });
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,RC_IMAGE_PICK);
+            }
+        });
+
+
+        jaja = (Spinner) findViewById(R.id.sp3);
 
         jaja.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, letra));
 
@@ -65,10 +110,10 @@ public class CrearIncidencia extends AppCompatActivity {
                     areas.add(areaName);
                 }
 
-                Spinner areaSpinner = (Spinner) findViewById(R.id.sp);
+                areaSpinner1 = (Spinner) findViewById(R.id.sp);
                 ArrayAdapter<String> areasAdapter = new ArrayAdapter<String>(CrearIncidencia.this, android.R.layout.simple_spinner_dropdown_item, areas);
                 areasAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                areaSpinner.setAdapter(areasAdapter);
+                areaSpinner1.setAdapter(areasAdapter);
             }
 
             @Override
@@ -87,10 +132,10 @@ public class CrearIncidencia extends AppCompatActivity {
                     areas.add(areaName);
 
 
-                Spinner areaSpinner = (Spinner) findViewById(R.id.sp2);
+                areaSpinner2 = (Spinner) findViewById(R.id.sp2);
                 ArrayAdapter<String> areasAdapter = new ArrayAdapter<String>(CrearIncidencia.this, android.R.layout.simple_spinner_item, areas);
                 areasAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                areaSpinner.setAdapter(areasAdapter);
+                areaSpinner2.setAdapter(areasAdapter);
             }
 
             @Override
@@ -99,4 +144,42 @@ public class CrearIncidencia extends AppCompatActivity {
             }
         });
     }
+
+    void uploadFile(){
+        StorageReference fileRef = FirebaseStorage.getInstance().getReference().child("incidencia/" + UUID.randomUUID());
+        fileRef.putFile(mediaUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                downloaderUrl = taskSnapshot.getDownloadUrl();
+                incidencia();
+            }
+        });
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void incidencia() {
+        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        String departamento = areaSpinner1.getTransitionName();
+        String destinatario = areaSpinner2.getTransitionName();
+        String motivo = jaja.getTransitionName();
+
+        if(downloaderUrl == null) {
+            mDatabase3.setValue(new Incidencia(user.getUid(), departamento, destinatario, motivo, null));
+        } else {
+            mDatabase3.setValue(new Incidencia(user.getUid(), departamento, destinatario, motivo, downloaderUrl.toString()));
+        }
+        finish();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data != null) {
+            if (requestCode == RC_IMAGE_PICK) {
+                mediaUri = data.getData();
+                Glide.with(this).load(mediaUri).into(image);
+            }
+        }
+    }
+
 }
