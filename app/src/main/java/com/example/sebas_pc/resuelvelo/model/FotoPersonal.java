01cda82be2c1fda3a62f6e668748c158a1b773fa
dambjs <dambjs@gmail.com>
2018-05-29 23:43:32 +0200

    Resuelvelo 1.0

--- app/src/main/java/com/example/sebas_pc/resuelvelo/model/FotoPersonal.java
+++ app/src/main/java/com/example/sebas_pc/resuelvelo/model/FotoPersonal.java
@@ -13,11 +13,12 @@ public class FotoPersonal {
 }
 
 
-//        1- Cuando se añade un departamento poder poner más de una persona
-//        2- Cada incidencia creada ha de ser por empresa.
-//        3- Al darle a otros en crear incidencia ha de salir un Edit text.
-//        4- El ver incidencia ha de ser por empresa y ademas al clicarle se ha de ver extensa.
-//        5- Spinner registrar empleado y que se muestre bien los datos.
-//        6- Eliminar empresas pulsando. --> Falta hacer un for.
-//        7- Perfil empresario mostrar las incidencias según su prioridad.
-//        8- Imagen de perfil empresarios.
+//        1- Cuando se añade un departamento poder poner más de una persona ---------------------------------------------------- HECHO
+//        2- Cada incidencia creada ha de ser por empresa. ---------------------------------------------------------------------
+//        3- Al darle a otros en crear incidencia ha de salir un Edit text. ---------------------------------------------------- HECHO
+//        4- El ver incidencia  al clicarle se ha de ver extensa. -------------------------------------------------------------- HECHO
+//        5- Spinner registrar empleado y que se muestre bien los datos.-------------------------------------------------------- medio HECHO
+//        6- Eliminar empresas pulsando. --------------------------------------------------------------------------------------- HECHO, mini fallo
+//        7- Perfil empresario mostrar las incidencias según su prioridad.------------------------------------------------------ medio
+//        8- Imagen de perfil empresarios.--------------------------------------------------------------------------------------
+//        9- Crear el has olvidado contraseña.----------------------------------------------------------------------------------
